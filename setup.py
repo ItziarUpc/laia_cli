c3505248473af2b117a4ae2b12db05c8def4fea7
Itziar Mensa Minguito <itziar@Itziars-MacBook-Air.local>
2025-10-31 09:27:09 +0100

    release: v0.2.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="laia-cli",
-    version="0.2.13",
+    version="0.2.14",
     description="CLI de la librería de LAIA",
     author="Itziar",
     author_email="itziar.mensa08@gmail.com",
